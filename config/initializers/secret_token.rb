de085629df861cebfae2a6822827419fc2c22c58
Vidur Murali <vidur.murali@gmail.com>
2012-03-22 19:21:10 -0400

    Created initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogs::Application.config.secret_token = '0b95e183940cb5f28f399bee4284b7dde6342f15ae8a187c4498728b1674c9af9cb2d0208347edbbf43bafaa3db1dd46dd2847c86a508ead70f95b7b7e3c8b88'
